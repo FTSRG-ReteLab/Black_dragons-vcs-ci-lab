39880d47dbab816cd1e83e40f4135c9d31c99d7d
meres <zihbot@gmail.com>
2019-04-08 07:20:52 -0700

    mocking

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -4,18 +4,37 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import static org.mockito.Mockito.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainUser;
+import hu.bme.mit.train.interfaces.TrainSensor;
+
+
+@RunWith(MockitoJUnitRunner.class)
 public class TrainSensorTest {
 
+    TrainSensor trainSensor;
+
+    @Mock
+    TrainController trainController;
+    @Mock
+    TrainUser trainUser;
 
     @Before
     public void before() {
-      TrainController trainController = mock(TrainController.class);
-      TrainUser trainUser = mock(TrainUser.class);
+      trainController = mock(TrainController.class);
+      trainUser = mock(TrainUser.class);
+
+      trainSensor = new TrainSensorImpl(trainController, trainUser);
+
+      when(trainController.getReferenceSpeed()).thenReturn(150);
     }
 
     @Test
     public void negativeSpeedLimit() {
-      when(trainController.setSpeedLimit(-5)).verify(trainUser).setAlarmState("called")
+      trainSensor.overrideSpeedLimit(550);
+      //verify(trainUser, atLeastOnce()).setAlarmState(true);
     }
 }
